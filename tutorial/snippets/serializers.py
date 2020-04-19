cf034df513d9ac88a6484b17aa07305d494b0086
Seung-Joon <tmdwns02556@gmail.com>
2020-04-19 15:19:46 +0900

    edit code using rest-framework

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -4,4 +4,10 @@ from snippets.models import Snippet
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style'] # set fields 
+        #fields = '__all__'  # set all fields
+        #exclude = 'title' # except fields
+        #read_only_fields = 'title' # set readonly field
+
+
+
